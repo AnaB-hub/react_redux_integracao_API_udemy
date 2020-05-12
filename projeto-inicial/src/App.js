2e70cf64fa4ba992693604b3085c501390f7f5ae
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-05-12 14:21:21 -0300

    State e Events

--- projeto-inicial/src/App.js
+++ projeto-inicial/src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Ana from './components/Ana';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Meu projeto em ReactJS
-        </p>
-        <Ana />
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      name: "Aninha"
+    }
+    this.changeState = this.changeState.bind(this)
+    this.resetState = this.resetState.bind(this)
+  }
+
+  changeState() {
+    this.setState({
+      name: "Ana B."
+    })
+  }
+
+  resetState() {
+    this.setState({
+      name: "Aninha"
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.name}
+        </div>
+        <div>
+          <button onClick={this.changeState}>Mudar nome</button>
+          <button onClick={this.resetState}>Resetar nome</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
