8cb3b07a66a3d6f19790216ef18a64de5871d46a
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-05-12 13:25:58 -0300

    Cria componente com meu nome, como exemplo

---
+++ projeto-inicial/src/ana.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Ana extends Component {
+  render() {
+    return (
+      <div className="Ana">
+        <header className="Ana-header">
+          <p>
+            Sou um componente chamado Ana
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Ana;
