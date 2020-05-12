9efe6f0b67176fcef42418ac98287d0963a74535
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-05-12 13:55:32 -0300

    Componentes e propriedades

--- projeto-inicial/src/components/Ana.js
+++ projeto-inicial/src/components/Ana.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import User from './User';
+import PhotoUser from '../assents/user.png';
+
 class Ana extends Component {
   render() {
     return (
       <div>
         Sou um componente chamado Ana
+        <User name="Ana B" photo={ PhotoUser } />
+        <User name="Ana"  />
+        <User name="Ana G" photo={ PhotoUser } />
       </div>
     );
   }
